04fdde5db609e919b7ff29c1c6b03c07786cfd19
niekvandermaas <58216209+niekvandermaas@users.noreply.github.com>
2023-10-04 14:22:30 +0200

    Update simulate_arrival_times function.R

--- simulate_arrival_times function.R
+++ simulate_arrival_times function.R
@@ -1,7 +1,7 @@
 simulate_arrival_times <- function(d_pt_month, n_patients) {
   
-  v_times <- cumsum(rexp(n_patients, d_pt_month))
-  return(v_times)
+  a_times <- cumsum(rexp(n_patients, d_pt_month))
+  return(a_times)
   
 }
 
